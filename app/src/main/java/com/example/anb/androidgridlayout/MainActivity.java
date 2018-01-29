9516335b735b4c1a09c26ce3ebd829e14dfe5007
Agung Nursatria <agungnursatria@gmail.com>
2018-01-29 23:53:43 +0700

    Add Clicked Action

--- app/src/main/java/com/example/anb/androidgridlayout/MainActivity.java
+++ app/src/main/java/com/example/anb/androidgridlayout/MainActivity.java
@@ -1,13 +1,63 @@
 package com.example.anb.androidgridlayout;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    GridLayout mainGrid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainGrid = (GridLayout) findViewById(R.id.mainGrid);
+
+        //Set Event
+        //setSingleEvent(mainGrid);
+        setToggleEvent(mainGrid);
+
+    }
+
+    private void setToggleEvent(GridLayout mainGrid) {
+        //Loop all child item of main grid
+        for (int i=0;i<mainGrid.getChildCount();i++){
+            final CardView cardView = (CardView) mainGrid.getChildAt(i);
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (cardView.getCardBackgroundColor().getDefaultColor() == -1){
+                        //Change Background color
+                        cardView.setBackgroundColor(Color.parseColor("#FF6F00"));
+                        Toast.makeText(MainActivity.this, "State : True", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        //Change Background color
+                        cardView.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                        Toast.makeText(MainActivity.this, "State : False", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            });
+        }
+    }
+
+    private void setSingleEvent(GridLayout mainGrid) {
+        //Loop all child item of main grid
+        for (int i=0;i<mainGrid.getChildCount();i++){
+            CardView cardView = (CardView) mainGrid.getChildAt(i);
+            final int finalI = i;
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, "Clicked at index " + finalI, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 }
